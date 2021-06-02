f18d0fbf1bcad7c4d1af348157500db4767e72fb
Divyansh Bhatia <divyansh.enest@gmail.com>
2021-06-03 00:03:24 +0530

    Add project files.

---
+++ Udp.Demo.Reciever/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Udp.Demo.Reciever
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Reciever started");
+
+			UdpClient udpServer = new UdpClient(11000);
+
+			while(true)
+			{
+				var remoteEp = new IPEndPoint(IPAddress.Any, 11000);
+				var data = udpServer.Receive(ref remoteEp);
+				Console.WriteLine("Recieved: " + Encoding.UTF8.GetString(data));
+			}
+		}
+	}
+}
---
+++ Udp.Demo.Sender/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Udp.Demo.Sender
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new UdpClient();
+
+			IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
+
+			client.Connect(endpoint);
+
+			byte[] bytes = Encoding.UTF8.GetBytes("Hello");
+
+			client.Send(bytes, bytes.Length);
+		}
+	}
+}
